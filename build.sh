ca73974e02d1ede64edc2a31304611ac58fdc0a5
Tao Chen <chentao904@163.com>
2018-10-17 20:52:52 -0400

    add binary vocabulary support

--- build.sh
+++ build.sh
@@ -20,7 +20,22 @@ cd ../../../
 echo "Uncompress vocabulary ..."
 
 cd Vocabulary
-tar -xf ORBvoc.txt.tar.gz
+TxtVocFile=./ORBvoc.txt
+TxtVocSize="145250924"
+if [ -e $TxtVocFile ]
+then
+    TXTVOCFILESIZE=$(stat -c%s "$TxtVocFile")
+    if [ $TXTVOCFILESIZE -eq $TxtVocSize ]
+    then
+        echo "Vocabulary(txt) already exists"
+    else
+        echo "Uncompress vocabulary ..."
+        tar -xf ORBvoc.txt.tar.gz
+    fi
+else
+    echo "Uncompress vocabulary ..."
+    tar -xf ORBvoc.txt.tar.gz
+fi
 cd ..
 
 echo "Configuring and building ORB_SLAM2 ..."
@@ -29,3 +44,21 @@ mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
+
+cd ..
+BinVocFile=./Vocabulary/ORBvoc.bin
+BinVocSize="44365017"
+if [ -e $BinVocFile ]
+then
+    BINVOCFILESIZE=$(stat -c%s "$BinVocFile")
+    if [ $BINVOCFILESIZE -eq $BinVocSize ]
+    then
+        echo "Vocabulary(bin) already exists"
+    else
+        echo "Recreating binary vocabulary"
+        ./tools/bin_vocabulary
+    fi
+else
+    echo "Creating binary vocabulary"
+    ./tools/bin_vocabulary
+fi
